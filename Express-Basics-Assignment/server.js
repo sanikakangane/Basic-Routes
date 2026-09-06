a39f9ba35a905a9e470e054166f7c9d5c8ec2eab
Sanika Kangane <sanikakangane166@gmail.com>
2026-09-06 13:34:39 +0530

    Add files via upload

---
+++ Express-Basics-Assignment/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const PORT = 7777;
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Home Page");
+})
+
+app.get("/about", (req, res) => {
+    res.send("This is About Page");
+})
+
+app.get("/contact", (req, res) => {
+    res.send("This is Contact Page");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}...`);
+})
