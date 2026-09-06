30be8b6c4940867529635f8361d26b173e76d3b4
Sanika Kangane <sanikakangane166@gmail.com>
2026-09-06 16:09:29 +0530

    Update server.js

--- Express-Basics-Assignment/server.js
+++ Express-Basics-Assignment/server.js
@@ -3,6 +3,11 @@ const app = express();
 
 const PORT = 7777;
 
+app.use((req, res, next) => {
+    console.log(req.method, req.url)
+    next();
+})
+
 app.get("/", (req, res) => {
     res.send("Welcome to Home Page");
 })
@@ -19,6 +24,14 @@ app.get("/user/:name", (req, res) => {
     res.send(`Hello ${req.params.name}`);
 })
 
+app.get("/product/:id/:category", (req, res) => {
+    res.send(`Product ID: ${req.params.id}, Category: ${req.params.category}`);
+})
+
+app.get("/search", (req, res) => {
+    res.send(`Name: ${req.query.name}, Role: ${req.query.role}`);
+})
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}...`);
+    console.log(`server is running on port ${PORT}...`)
 })
